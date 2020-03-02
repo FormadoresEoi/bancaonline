411436bc1e771219b5df1ddb5783eb47c46f9a2f
Lopez2719 <franciscolopez2719@gmail.com>
2020-03-02 20:35:39 +0100

    ServiceAmortizacion
    faltan los demás hulio

--- MundoBancario/src/main/java/es/eoi/mundobancario/Service/AmortizacionService.java
+++ MundoBancario/src/main/java/es/eoi/mundobancario/Service/AmortizacionService.java
@@ -1,5 +1,16 @@
 package es.eoi.mundobancario.Service;
 
+import java.util.Optional;
+
+import es.eoi.mundobancario.entity.Amortizacion;
+
 public interface AmortizacionService {
 
+	Amortizacion CreateAmortizacion(Amortizacion amortizacion);
+
+	Optional<Amortizacion> findAmortizacionById(int id);
+
+	Amortizacion updateAmortizacion(Amortizacion amortizacion);
+
+	void removeAmortizacion(int id);
 }
--- MundoBancario/src/main/java/es/eoi/mundobancario/Service/AmortizacionServiceImpl.java
+++ MundoBancario/src/main/java/es/eoi/mundobancario/Service/AmortizacionServiceImpl.java
@@ -1,8 +1,38 @@
 package es.eoi.mundobancario.Service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.eoi.mundobancario.entity.Amortizacion;
+import es.eoi.mundobancario.Repository.AmortizacionRepository;
 
 @Service
 public class AmortizacionServiceImpl implements AmortizacionService {
 
+	@Autowired
+	AmortizacionRepository amortizacionrepository;
+
+	@Override
+	public Amortizacion CreateAmortizacion(Amortizacion amortizacion) {
+		return amortizacionrepository.save(amortizacion);
+	}
+
+	@Override
+	public Optional<Amortizacion> findAmortizacionById(int id) {
+
+		return amortizacionrepository.findById(id);
+
+	}
+
+	@Override
+	public Amortizacion updateAmortizacion(Amortizacion amortizacion) {
+		return amortizacionrepository.save(amortizacion);
+	}
+
+	@Override
+	public void removeAmortizacion(int id) {
+		amortizacionrepository.deleteById(id);
+	}
 }
