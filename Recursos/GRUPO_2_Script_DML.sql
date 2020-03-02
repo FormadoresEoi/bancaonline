00a2d93e513e5f8625911c9f3ad864b4f25b70d3
navalon96 <61698601+navalon96@users.noreply.github.com>
2020-03-02 18:02:39 +0100

    Add files via upload

---
+++ Recursos/GRUPO_2_Script_DML.sql
@@ -0,0 +1,5 @@
+insert into TIPOS_MOVIMIENTO values (0,'INGRESO');
+insert into TIPOS_MOVIMIENTO values (1,'PRESTAMO');
+insert into TIPOS_MOVIMIENTO values (2,'PAGO');
+insert into TIPOS_MOVIMIENTO values (3,'AMORTIZACION');
+insert into TIPOS_MOVIMIENTO values (4,'INTERES');
