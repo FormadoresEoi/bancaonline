c7955af5868716e6ec8d0086f01cb739acea6d9a
Jorge <navalon96@gmail.com>
2020-03-02 18:06:21 +0100

    correcion

--- Recursos/GRUPO_2_Script_DML.sql
+++ Recursos/GRUPO_2_Script_DML.sql
@@ -1,5 +1,5 @@
 insert into TIPOS_MOVIMIENTO values (0,'INGRESO');
 insert into TIPOS_MOVIMIENTO values (1,'PRESTAMO');
 insert into TIPOS_MOVIMIENTO values (2,'PAGO');
-insert into TIPOS_MOVIMIENTO values (3,'AMOTIZACION');
+insert into TIPOS_MOVIMIENTO values (3,'AMORTIZACION');
 insert into TIPOS_MOVIMIENTO values (4,'INTERES');
