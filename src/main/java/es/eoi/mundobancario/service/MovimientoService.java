caac38b5c9c9961f11a6c4ea41f14f7e8de0e075
manulaiko <manulaiko@gmail.com>
2020-03-02 18:38:43 +0100

    Create services

---
+++ src/main/java/es/eoi/mundobancario/service/MovimientoService.java
@@ -0,0 +1,14 @@
+package es.eoi.mundobancario.service;
+
+import es.eoi.mundobancario.entity.Movimiento;
+
+/**
+ * Movimiento service.
+ * ===================
+ *
+ * Service for the Movimiento repository.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public interface MovimientoService extends IService<Movimiento, Integer> {
+}
---
+++ src/main/java/es/eoi/mundobancario/service/PrestamoService.java
@@ -0,0 +1,14 @@
+package es.eoi.mundobancario.service;
+
+import es.eoi.mundobancario.entity.Prestamo;
+
+/**
+ * Prestamo service.
+ * =================
+ *
+ * Service for the Prestamo repository.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public interface PrestamoService extends IService<Prestamo, Integer> {
+}
