559c04241a503573f5329a88267bb52f23709033
jose <jose.forteaquiles@gmail.com>
2020-03-02 20:19:25 +0100

    controller cliente

--- bancaonline/src/main/java/es/eoi/mundobancario/controller/ClientesController.java
+++ bancaonline/src/main/java/es/eoi/mundobancario/controller/ClientesController.java
@@ -1,10 +1,73 @@
 package es.eoi.mundobancario.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.eoi.mundobancario.dto.ClienteDto;
+import es.eoi.mundobancario.entity.Cliente;
+import es.eoi.mundobancario.service.ClienteService;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClientesController {
+
+	@Autowired
+	private ClienteService service;
+
+	@Autowired
+	private ModelMapper model;
+
+	@PostMapping
+	public ResponseEntity<String> create(@RequestBody ClienteDto dto) {
+		if (!service.find(dto.getId()).get().equals(null))
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		else {
+			service.create(model.map(dto, Cliente.class));
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		}
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClienteDto> find(@PathVariable int id) {
+		Optional<Cliente> cliente = service.find(id);
+		if (!cliente.isPresent())
+			return new ResponseEntity<ClienteDto>(HttpStatus.NOT_FOUND);
+		ClienteDto dto = model.map(cliente.get(), ClienteDto.class);
+		return new ResponseEntity<ClienteDto>(dto, HttpStatus.OK);
+	}
+
+	@GetMapping
+	public List<ClienteDto> findAll() {
+		List<ClienteDto> clientes = service.findAll().stream().map(c -> model.map(c, ClienteDto.class))
+				.collect(Collectors.toList());
+
+		return clientes;
+	}
+
+	@PutMapping(value = "/{id}")
+	public void update(@RequestBody ClienteDto dto) {
+		Cliente cliente = model.map(dto, Cliente.class);
+		service.updateCliente(cliente);
+	}
+	
+
+	@DeleteMapping("/{id}")
+	public void remove(@PathVariable int id) {
+		service.remove(id);
+	}
 
 }
--- bancaonline/src/main/java/es/eoi/mundobancario/controller/CuentasController.java
+++ bancaonline/src/main/java/es/eoi/mundobancario/controller/CuentasController.java
@@ -1,10 +1,19 @@
 package es.eoi.mundobancario.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.eoi.mundobancario.service.ClienteService;
+import es.eoi.mundobancario.service.CuentaService;
 
 @RestController
 @RequestMapping("/cuentas")
 public class CuentasController {
-
+	
+	@Autowired
+	private CuentaService cuentaService;
+	
+	@Autowired
+	private ClienteService clienteService;
 }
