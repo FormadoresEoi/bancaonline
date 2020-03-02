6cb88279d02270f1a83ff5b7658448a59d98723e
carloslozano55 <carlos.bernabeu25@gmail.com>
2020-03-02 20:41:02 +0100

    ClienteService Actualizado

--- Grupo3Banco/src/main/java/es/eoi/mundobancario/service/ClienteService.java
+++ Grupo3Banco/src/main/java/es/eoi/mundobancario/service/ClienteService.java
@@ -1,5 +1,25 @@
 package es.eoi.mundobancario.service;
 
-public interface ClienteService {
+import java.util.List;
 
+import es.eoi.mundobancario.entity.Cliente;
+
+public interface ClienteService {
+	
+	
+	//Encontrar todos los Clientes
+	
+	List<Cliente> findAll();
+	
+	//Encontrar Cliente por ID
+	
+	Cliente findById (int id);
+	
+	//Crear Cliente
+	
+	void createClient(Cliente cliente);
+	
+	//Update Cliente
+	
+	void updateClient(Cliente cliente);
 }
