4c00c5929de8b046877ace56e1bc55169c5e5657
AnaULAB <ana.aguilar01@goumh.umh.es>
2020-03-02 19:54:13 +0100

    prueba  ana

--- MundoBancario/src/main/java/es/eoi/mundobancario/MundoBancarioApplication.java
+++ MundoBancario/src/main/java/es/eoi/mundobancario/MundoBancarioApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MundoBancarioApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args  ) {
 		SpringApplication.run(MundoBancarioApplication.class, args);
 	}
 
