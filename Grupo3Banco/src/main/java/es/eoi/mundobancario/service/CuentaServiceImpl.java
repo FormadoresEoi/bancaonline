e0e3beb81a9b417d2266cde0b4dc900996e6a59e
SergioGitHub8 <vizka811@gmail.com>
2020-03-02 20:42:20 +0100

    Cuenta service

--- Grupo3Banco/src/main/java/es/eoi/mundobancario/service/CuentaServiceImpl.java
+++ Grupo3Banco/src/main/java/es/eoi/mundobancario/service/CuentaServiceImpl.java
@@ -1,5 +1,12 @@
 package es.eoi.mundobancario.service;
 
-public class CuentaServiceImpl implements CuentaService{
+import org.springframework.beans.factory.annotation.Autowired;
 
+import es.eoi.mundobancario.repository.CuentaRepository;
+
+
+
+public class CuentaServiceImpl implements CuentaService {
+	@Autowired
+	CuentaRepository repository;
 }
