bec63d26115522181a9b75270d0eb86951f0eede
Carlos Sanchez <karlos.sangar@gmail.com>
2020-03-03 16:53:03 +0100

    add ClienteController

--- src/main/java/es/eoi/mundobancario/controller/ClienteController.java
+++ src/main/java/es/eoi/mundobancario/controller/ClienteController.java
@@ -1,31 +1,48 @@
 package es.eoi.mundobancario.controller;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import es.eoi.mundobancario.entity.Cliente;
+import es.eoi.mundobancario.dto.ClienteDto;
 import es.eoi.mundobancario.service.ClienteService;
+import lombok.RequiredArgsConstructor;
 
+/**
+ * Cliente controller ===============
+ * 
+ * Controller for the ClienteDTO.
+ * 
+ * @author Carlos Sanchez <karlos.sangar@gmail.com>
+ */
+
+@RequiredArgsConstructor
+@RequestMapping
 @RestController
 public class ClienteController {
 
-	@Autowired
-	ClienteService service;
+	private final ModelMapper mapper = new ModelMapper();
+
+	private final ClienteService service = null;
+
+//	@GetMapping("/clientes")
+//	public List<Cliente> findAll() {
+//		return service.find();
+//	}
 
 	@GetMapping("/clientes")
-	public List<Cliente> findAll() {
-		return service.find();
+	public ClienteDto find() {
+		return mapper.map(service.find(), ClienteDto.class);
 	}
 
 	@GetMapping("/clientes/{id}")
-	public Optional<Cliente> findCliente(@PathVariable int id) {
-		return service.find(id);
-
+	public ClienteDto findById(@RequestBody int id) {
+		return mapper.map(service.find(id), ClienteDto.class);
 	}
 
 //    @PostMapping("/clientes/login")
