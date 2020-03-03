943425b2174c087a630f81124a3febb69bdc4886
Lopez2719 <franciscolopez2719@gmail.com>
2020-03-03 20:49:34 +0100

    Auto stash before merge of "g2_develop" and "origin/g2_develop"

--- MundoBancario/src/main/java/es/eoi/mundobancario/Repository/ClientesRepository.java
+++ MundoBancario/src/main/java/es/eoi/mundobancario/Repository/ClientesRepository.java
@@ -1,11 +1,18 @@
 package es.eoi.mundobancario.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import es.eoi.mundobancario.entity.Cliente;
 
 @Repository
 public interface ClientesRepository extends JpaRepository<Cliente, Integer> {
+
+	@Query("select * from clientes where clientes.nombre = :nombre and clientes.pass = :pass ")
+	Cliente login(
+	@Param("usuario") String usuario,
+	@Param("pass") String pass);
 
 }
