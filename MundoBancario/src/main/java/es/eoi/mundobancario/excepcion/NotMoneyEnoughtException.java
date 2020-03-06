5b5804de031c47a97585c5337237664075ed5797
Jorge <navalon96@gmail.com>
2020-03-06 20:40:23 +0100

    commit

--- MundoBancario/src/main/java/es/eoi/mundobancario/excepcion/NotMoneyEnoughtException.java
+++
@@ -1,9 +0,0 @@
-package es.eoi.mundobancario.excepcion;
-
-@SuppressWarnings("serial")
-public class NotMoneyEnoughtException extends Exception {
-	
-	public NotMoneyEnoughtException() {
-		super("No queda dinero suficiente en la cuenta");
-	}
-}
--- MundoBancario/src/main/java/es/eoi/mundobancario/util/Reports.java
+++
@@ -1,7 +0,0 @@
-package es.eoi.mundobancario.util;
-
-public class Reports {
-	public void toPdf() {
-		
-	}
-}
